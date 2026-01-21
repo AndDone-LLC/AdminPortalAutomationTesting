7601d6e33dfde8316356fcaaf79e425af383b2a9
Siddheshwar Anajekar <siddheshwar.anajekar@aurionpro.com>
2026-01-21 11:45:49 +0530

    Added fileCommonUtils

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // In common-base/src/index.ts
-export { BasePage } from './pages/BasePage';
-export { commonUtils } from './utils/commonUtils';
-export { AssertionUtils } from './utils/assertionUtils';
-export { GenerationUtils } from './utils/generationUtils';
-export { tableUtils } from './utils/tableUtils';
-export { ValidationUtils } from './utils/validationUtils';
+export { BasePage } from "./pages/BasePage";
+export { commonUtils } from "./utils/commonUtils";
+export { AssertionUtils } from "./utils/assertionUtils";
+export { GenerationUtils } from "./utils/generationUtils";
+export { tableUtils } from "./utils/tableUtils";
+export { ValidationUtils } from "./utils/validationUtils";
+export { fileCommonUtils } from "./utils/fileCommonUtils";
