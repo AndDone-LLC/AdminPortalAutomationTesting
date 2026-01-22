718259efc1c8c3e625ded95a25772b91a4999f0a
Nikhil Deshpande <nikhil.deshpande@asl.mum.sst>
2026-01-22 14:57:12 +0530

    Modified exisiting methods , click. fill, getText make it static and handle exceptions
    Added class tableUtils.ts new methods for table utilities methods

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -16,8 +16,10 @@ export class commonUtils {
     return value;
   }
 
-  //   
-  private async highlight(locator: Locator) {
+  /**
+  * Highlights a UI element on the page for debugging and visual validation.
+  */
+  private static async highlight(page: Page, locator: Locator) {
     try {
       const attached = await locator.isVisible({ timeout: 3000 }).catch(() => false);
       if (!attached) return;
@@ -25,11 +27,9 @@ export class commonUtils {
       const handle = await locator.elementHandle();
       if (!handle) return;
 
-      // Change (el: HTMLElement) to (el: any) to bypass strict property checking in the browser context
-      await this.page.evaluate((el: any) => {
+      await page.evaluate((el: any) => {
         const orig = el.getAttribute('style') || '';
 
-        // Use style.setProperty or direct assignment
         el.style.transition = 'box-shadow 0.95s ease';
         el.style.boxShadow = '0 0 15px 5px rgba(231, 12, 23, 0.9)';
 
@@ -43,34 +43,112 @@ export class commonUtils {
     }
   }
 
-  async click(locator: Locator, timeout = 15000) {
-    await locator.waitFor({ state: 'visible', timeout }).catch(() => { });
-    await this.highlight(locator);
-    await locator.click({ timeout });
+  /**
+   * Clicks on a web element after waiting for it to become visible.
+   * 
+   * If the element is not found or the click fails, the error is logged
+   * 
+   * @param page - Playwright Page object
+   * @param locator - Locator of the element to be clicked
+   * @param timeout - Maximum wait time in milliseconds (default: 15000)
+   */
+  static async click(page: Page, locator: Locator, timeout: number = 15000): Promise<void> {
+    try {
+      await locator.waitFor({ state: 'visible', timeout });
+      await this.highlight(page, locator);
+      await locator.click({ timeout });
+    } catch (error) {
+      console.warn(`⚠ Click skipped: ${(error as Error).message}`
+      );
+    }
+  }
+  /**
+   * Attempts to fill text into an input field.
+   * If the element is not visible, detached, or the fill action fails,
+   * the error is logged and execution continues.
+   * 
+   * @param page - Playwright Page object
+   * @param locator - Locator of the element to be clicked
+   * @param timeout - Maximum wait time in milliseconds (default: 15000)
+   */
+  static async fill(page: Page, locator: Locator, text: string, timeout: number = 15000
+  ): Promise<void> {
+    try {
+      await locator.waitFor({ state: 'visible', timeout });
+      await this.highlight(page, locator);
+      await locator.fill(text);
+    } catch (error) {
+      console.warn(
+        `⚠ Fill skipped for element. Reason: ${(error as Error).message}`
+      );
+    }
   }
 
-  async fill(locator: Locator, text: string, timeout = 15000) {
-    await locator.waitFor({ state: 'visible', timeout }).catch(() => { });
-    await this.highlight(locator);
-    await locator.fill(text);
+  /**
+  * Types text into an input field using keyboard events..
+  *
+  * If the element is not ready or typing fails, the error is logged
+  * and test execution continues.
+  * 
+  * @param page - Playwright Page object
+  * @param locator - Locator of the input or editable element
+  * @param text - Text to be typed
+  * @param timeout - Maximum wait time in milliseconds (default: 15000)
+  *
+  */
+  static async typeText(page: Page, locator: Locator, text: string, timeout: number = 15000
+  ): Promise<void> {
+    try {
+      await locator.waitFor({ state: 'visible', timeout });
+      await this.highlight(page, locator);
+      await locator.type(text, { delay: 50 });
+    } catch (error) {
+      console.warn(
+        `⚠ Type skipped for element. Reason: ${(error as Error).message}`
+      );
+    }
   }
 
-  async type(locator: Locator, text: string, timeout = 15000) {
-    await locator.waitFor({ state: 'visible', timeout }).catch(() => { });
-    await this.highlight(locator);
-    await locator.type(text);
+  /**
+   * Waits for an element to become visible
+   * 
+   * @param locator - Locator of the element to wait for
+   * @param timeout - Maximum wait time in milliseconds (default: 15000)
+   * @param description - Readable description used in logs
+   * 
+   * */
+  static async waitForVisible(locator: Locator, timeout: number = 15000, description = 'Element'
+  ): Promise<void> {
+    try {
+      await locator.waitFor({ state: 'visible', timeout });
+    } catch (error) {
+      console.error(`❌ ${description} not visible after ${timeout}ms`);
+    }
   }
 
-  async waitForVisible(locator: Locator, timeout = 10000) {
-    await locator.waitFor({ state: 'visible', timeout });
-  }
+  /**
+  * This method retrieves visible text from a locator.
+  *
+  * @param locator - Playwright locator from which text is retrieved
+  * @param timeout - Maximum wait time for visibility (default: 10000ms)
+  * @param description - Logical name of the element for logging
+  *
+  * @returns The extracted text or empty string if unavailable
+  *
+ */
+  static async getText(locator: Locator, timeout: number = 10000, description: string = 'Element'
+  ): Promise<string> {
+    try {
+      await locator.waitFor({ state: 'visible', timeout }).catch(() => { });
 
-  async getText(locator: Locator): Promise<string> {
-    const text = await locator.textContent().catch(() => '');
-    return text ?? '';
-  }
+      const text = await locator.innerText().catch(() => '');
 
- 
+      return text.trim();
+    } catch {
+      console.warn(`⚠ Unable to get text from ${description}`);
+      return '';
+    }
+  }
   /**
    * Checks if the given list is sorted in ascending order.
    * @param list List of comparable values
@@ -124,13 +202,13 @@ export class commonUtils {
 
     return order === 'asc' ? sorted : sorted.reverse();
   }
- /**
-   * Sort date strings from Newest to Oldest using given format.
-   * @param dateStrings Date strings from UI
-   * @param format Date format (e.g. 'MMM dd, yyyy HH:mm:s')
-   * @returns Sorted date strings (Newest first)
-   */
-  static sortDateNewestToOldest(dateStrings: string[],format: string): string[] {
+  /**
+    * Sort date strings from Newest to Oldest using given format.
+    * @param dateStrings Date strings from UI
+    * @param format Date format (e.g. 'MMM dd, yyyy HH:mm:s')
+    * @returns Sorted date strings (Newest first)
+    */
+  static sortDateNewestToOldest(dateStrings: string[], format: string): string[] {
     if (!dateStrings || dateStrings.length === 0) return [];
 
     const dates = dateStrings.map(d =>
@@ -148,7 +226,7 @@ export class commonUtils {
    * @param format Date format (e.g. 'MMM dd, yyyy HH:mm:s')
    * @returns Sorted date strings (Oldest first)
    */
-  static sortDateOldestToNewest(dateStrings: string[],format: string): string[] {
+  static sortDateOldestToNewest(dateStrings: string[], format: string): string[] {
     if (!dateStrings || dateStrings.length === 0) return [];
 
     const dates = dateStrings.map(d =>
@@ -160,22 +238,22 @@ export class commonUtils {
     return dates.map(d => this.formatDateGeneric(d, format));
   }
 
- /**
-   * Month name to month index mapping for parsing 'MMM' format.
-   */
+  /**
+    * Month name to month index mapping for parsing 'MMM' format.
+    */
   static readonly MONTH_MAP: Record<string, number> = {
     Jan: 0, Feb: 1, Mar: 2, Apr: 3,
     May: 4, Jun: 5, Jul: 6, Aug: 7,
     Sep: 8, Oct: 9, Nov: 10, Dec: 11
   }
 
-    /**
-   * Parses a date string into a Date object using a generic format.
-   * Supports multiple date tokens like yyyy, MM, MMM, dd, HH, mm, ss.
-   * @param dateStr Date string from UI
-   * @param format Format of the date string
-   * @returns Parsed Date object
-   */
+  /**
+ * Parses a date string into a Date object using a generic format.
+ * Supports multiple date tokens like yyyy, MM, MMM, dd, HH, mm, ss.
+ * @param dateStr Date string from UI
+ * @param format Format of the date string
+ * @returns Parsed Date object
+ */
   static parseDateGeneric(dateStr: string, format: string): Date {
     const tokenRegex = /(yyyy|MMM|MM|dd|HH|mm|ss|s)/g;
     const formatTokens = format.match(tokenRegex);
@@ -205,12 +283,12 @@ export class commonUtils {
     return new Date(year, month, day, hour, minute, second);
   }
 
-   /**
-   * Formats a Date object into a string using the provided format.
-   * @param date Date object
-   * @param format Desired output format
-   * @returns Formatted date string
-   */
+  /**
+  * Formats a Date object into a string using the provided format.
+  * @param date Date object
+  * @param format Desired output format
+  * @returns Formatted date string
+  */
   static formatDateGeneric(date: Date, format: string): string {
     const pad = (n: number) => n.toString().padStart(2, "0");
 
